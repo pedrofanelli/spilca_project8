2be5bfb5534066eaefba1701c3d635bb763578f8
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-10-16 13:48:21 -0300

    Project 8

---
+++ src/test/java/org/example/spilca_project8/AppTest.java
@@ -0,0 +1,20 @@
+package org.example.spilca_project8;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
